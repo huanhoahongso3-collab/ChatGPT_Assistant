132b0fc095d541aa229cdb67a187604c1dfd84f3
Proxlight <69844328+Proxlight@users.noreply.github.com>
2023-06-05 11:49:14 +0530

    Create chatgui.py
    In this example, replace `"YOUR_API_KEY"` with your actual OpenAI API key. Make sure you have the OpenAI Python library installed (`pip install openai`) and you have an active OpenAI API key.
    
    The `send_message()` function is called when the user clicks the "Send" button. It retrieves the message from the entry field, adds it to the chat box as a user message, generates a response using the ChatGPT model, and adds the response to the chat box.
    
    The `generate_response()` function makes an API call to OpenAI's completion endpoint using the OpenAI Python library. It sends the user's message as the prompt and retrieves a response from the ChatGPT model. Adjust the parameters of the API call (`max_tokens`, `temperature`, etc.) based on your desired behavior.
    
    The main window is created using `tk.Tk()`, and the chat box and entry field are added using `tk.Text()` and `tk.Entry()`, respectively. The "Send" button is created using `tk.Button()`, and its command is set to the `send_message()` function.
    
    To run the application, save the code in a Python file (e.g., `chatgui.py`) and run it using the Python interpreter:
    
    ```
    python chatgui.py
    ```
    
    You should see a window with a chat box, an entry field, and a "Send" button. Type a message in the entry field and press Enter or click the "Send" button to see the response from the ChatGPT model displayed in the chat box.

---
+++ chatgui.py
@@ -0,0 +1,43 @@
+import tkinter as tk
+import openai
+
+# Set up OpenAI API credentials
+openai.api_key = "YOUR_API_KEY"
+
+# Function to handle sending a message
+def send_message():
+    message = entry.get()
+    if message.strip() != "":
+        chat_box.insert(tk.END, "User: " + message + "\n")
+        response = generate_response(message)
+        chat_box.insert(tk.END, "ChatGPT: " + response + "\n")
+        entry.delete(0, tk.END)
+
+# Function to generate a response using ChatGPT model
+def generate_response(message):
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=message,
+        max_tokens=50,
+        temperature=0.7
+    )
+    return response.choices[0].text.strip()
+
+# Create the main window
+window = tk.Tk()
+window.title("ChatGPT GUI")
+
+# Create a chat box
+chat_box = tk.Text(window, width=50, height=20)
+chat_box.grid(column=0, row=0, padx=10, pady=10)
+
+# Create an entry field for user input
+entry = tk.Entry(window, width=40)
+entry.grid(column=0, row=1, padx=10, pady=10)
+
+# Create a button to send the message
+button = tk.Button(window, text="Send", command=send_message)
+button.grid(column=1, row=1, padx=10, pady=10)
+
+# Start the GUI main loop
+window.mainloop()
